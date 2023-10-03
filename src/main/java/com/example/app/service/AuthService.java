79cf2186f2d5251f1d39c086dfd5f3a5f5855e67
Agis878 <a.lukasik@op.pl>
2023-10-03 15:02:27 +0200

    Spring Security. Authentication Service

---
+++ src/main/java/com/example/app/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.example.app.service;
+
+
+import com.example.app.model.User;
+import com.example.app.repositories.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.getByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("No username " + username + " found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(), user.getActive(), true, true, true,
+                Set.of(new SimpleGrantedAuthority(user.getRole())));
+    }
+}
